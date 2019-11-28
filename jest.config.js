70161c877e2d2c01a7b1cf1d1d7ebdf31868ecfb
Marko Marinovic <marinovic.marko@outlook.com>
2019-11-28 21:43:15 +0100

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,27 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!**/node_modules/**'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx,js,jsx}",
+    "!**/node_modules/**"
+  ],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)?$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json"
+  ],
+  testPathIgnorePatterns: ["/node_modules/"],
+  moduleDirectories: [
+    "node_modules",
+    mapFromRootDir('src')
+  ],
   moduleNameMapper: {
-    '\\.(scss)$': '<rootDir>/src/helpers/test/mocks/scss_mock.js',
-    '\\.(svg)$': '<rootDir>/src/helpers/test/mocks/svg_mock.js',
-  },
+    "\\.(scss)$": mapFromRootDir("src/helpers/test/mocks/scss_mock.js"),
+    "\\.(svg)$": mapFromRootDir("src/helpers/test/mocks/svg_mock.js"),
+  }
 };
+
+const mapFromRootDir = (path) => '<rootDir>/' + path;
